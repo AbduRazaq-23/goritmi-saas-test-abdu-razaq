38f414a332ed1a24dfc9463c7821d8c3e54338a1
Abdurazaq333 <irazaqr986@gmail.com>
2025-12-19 17:35:17 +0500

    update the message to return

--- goritmi-backend/src/middlewares/rateLimit.middleware.js
+++ goritmi-backend/src/middlewares/rateLimit.middleware.js
@@ -3,7 +3,12 @@ import rateLimit from "express-rate-limit";
 const resendOtpLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, //5 mint
   max: 3,
-  message: "Too many OTP requests. Please try again later.",
+  handler: (req, res) => {
+    return res.status(429).json({
+      success: false,
+      message: "Too many OTP requests. Please try again after 5 minutes.",
+    });
+  },
 });
 
 export default resendOtpLimiter;
