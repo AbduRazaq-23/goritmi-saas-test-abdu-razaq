f769297c8633e7fb62926fcb947d7de400c09809
Abdurazaq333 <irazaqr986@gmail.com>
2026-01-02 17:27:44 +0500

    add drop menu of profile

--- goritmi-frontend/src/Components/Dashboard/TopBar.jsx
+++ goritmi-frontend/src/Components/Dashboard/TopBar.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { FaFileInvoiceDollar } from "react-icons/fa";
 import axios from "axios";
 
 const TopBar = () => {
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef(null);
+
   const [length, setLength] = useState(null);
   const { user, loading } = useAuth();
 
@@ -23,19 +26,25 @@ const TopBar = () => {
       fetchMyInvoices();
     }, []);
 
-  return (
-    <header className="sticky top-0 w-full z-50 bg-gray-900 text-gray-100 border-b p-4 flex items-center justify-between">
-      <div className="flex items-center gap-4">
-        <div className="text-sm ">
-          Welcome back,{" "}
-          <span className="font-medium text-gray-300">
-            {loading ? "Loading..." : user?.name || "User"}
-          </span>
-        </div>
-      </div>
+  // click outside
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
 
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <header className="sticky top-0 w-full z-50 bg-gray-900 text-gray-100 border-b p-4 flex items-center justify-end">
       {/* BADGE & LOGO  */}
-      <div className="flex items-center gap-5">
+      <div className=" flex items-center gap-5">
         {/* if user show badge  */}
         {isUser && (
           <div className="relative inline-block">
@@ -62,8 +71,33 @@ const TopBar = () => {
             </span>
           </div>
         )}
-        <div className="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center text-white font-bold">
-          {user?.name?.[0]?.toUpperCase() || "U"}
+
+        <div ref={menuRef} className="relative">
+          <div
+            onClick={() => setOpen(!open)}
+            className="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center text-white font-bold"
+          >
+            <img
+              className="h-10 w-10 rounded-full"
+              src={user?.logo.url}
+              alt="logo"
+            />
+          </div>
+          {open && (
+            <div className="absolute right-0 text-center w-48 bg-gray-900 border rounded-md shadow-lg  z-50">
+              <img
+                className="rounded-full w-14 h-14 mx-auto mt-5"
+                src={user?.logo.url}
+                alt="logo"
+              />
+              <div className="px-4 py-2 text-sm text-gray-200">
+                <p className="font-semibold">{user?.name}</p>
+                <p className="text-xs text-gray-300">{user?.email}</p>
+              </div>
+
+              {/*  */}
+            </div>
+          )}
         </div>
       </div>
     </header>
